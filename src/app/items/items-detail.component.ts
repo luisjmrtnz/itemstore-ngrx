c37bc689c44df8acffb5a2483c6bd330bfa08854
luisjmartnz <luis_martin_24@hotmail.com>
2017-01-16 15:34:29 -0400

    Showing Details Functionality

--- src/app/items/items-detail.component.ts
+++ src/app/items/items-detail.component.ts
@@ -7,6 +7,21 @@ import { Item } from '../common/models/item.model';
     template:`
         <div>
             <h2 *ngIf="selectedItem.id">Editing {{originalName}}</h2>
+            <h2 *ngIf="!selectedItem.id">Create new Item</h2>
+        </div>
+        <div>
+            <div>
+                <label>Item name: </label>
+                <input type="text" 
+                       placeholder="Enter the name"
+                       [(ngModel)]="selectedItem.name">
+            </div>
+            <div>
+                <label>Item description: </label>
+                <input type="text" 
+                       placeholder="Enter the description"
+                       [(ngModel)]="selectedItem.description">
+            </div>
         </div>
     `
 })
