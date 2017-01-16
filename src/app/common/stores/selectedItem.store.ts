43c147588fda68082d64baeeb98d8003aed27512
luisjmartnz <luis_martin_24@hotmail.com>
2017-01-16 01:46:23 -0400

    Finishing the reducers

--- src/app/common/stores/selectedItem.store.ts
+++ src/app/common/stores/selectedItem.store.ts
@@ -1,5 +1,7 @@
 export const selectedItem = (state: any = null, {type, payload}) => {
     switch(type){
+        case 'SELECTE_ITEM':
+            return payload;
         default:
             return state;
     }
