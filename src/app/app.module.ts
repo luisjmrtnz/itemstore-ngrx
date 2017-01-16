38e0eaa4bde840d04af5792e7550b2c0c903211f
luisjmartnz <luis_martin_24@hotmail.com>
2017-01-16 00:56:44 -0400

    Importing and initilizing StoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
 
+import { items } from './common/stores/items.store';
+import { selectedItem } from './common/stores/selectedItem.store';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore({ items, selectedItem })
   ],
   providers: [],
   bootstrap: [AppComponent]
